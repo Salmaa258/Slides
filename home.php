7710397ad57a0168a7a4de5131255c42d413abd3
Emilio Fernández Vicente <efernandez@alumnat.copernic.cat>
2023-10-24 20:40:00 +0200

    eliminadas funciones hover preview

--- home.php
+++ home.php
@@ -10,7 +10,7 @@
 
 <body>
     <h2>Presentaciones</h2>
-        <div id="global">
+    <div id="global">
         <div id="añadirPresentacion">
             <a href="html/crear_p.html">
                 <div class="caja_añadir">
@@ -19,40 +19,45 @@
             </a>
             <p id="añadir">AÑADIR</p>
         </div>
-            <?php
-            // Punto de entrada principal
-            define('VALID_ENTRY_POINT', true);
+        <?php
+        // Punto de entrada principal
+        define('VALID_ENTRY_POINT', true);
 
-            include 'php/config.php';
+        include 'php/config.php';
 
-            // Incluir la clase de base de datos
-            require_once ROOT_PATH . 'php/clases/db.php';
+        // Incluir la clase de base de datos
+        require_once ROOT_PATH . 'php/clases/db.php';
 
-            // Obtener la única instancia de la base de datos
-            $db = Database::getInstance();
+        // Obtener la única instancia de la base de datos
+        $db = Database::getInstance();
 
-            // Obtener la conexión a la base de datos
-            $conn = $db->getConnection();
+        // Obtener la conexión a la base de datos
+        $conn = $db->getConnection();
 
-            // Consultar todas las presentaciones
-            $query = "SELECT id, titulo FROM presentacion";
-            $result = $conn->query($query);
+        // Consultar todas las presentaciones
+        $query = "SELECT id, titulo FROM presentacion";
+        $result = $conn->query($query);
 
-            // Generar dinámicamente los divs con class="caja"
-            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-                echo '<div class="caja" onmouseover="mostrarImagen(this)" onmouseout="ocultarImagen(this)">';
-                echo '<p>' . htmlspecialchars($row['titulo']) . '</p>';
-                echo '<button class="opciones-btn" onclick="mostrarOpciones(event)">+</button>';
-                echo '<div class="opciones" id="opciones' . $row['id'] . '">';
-                echo '<button class="editar"><img src="icons/editar.svg" alt="Editar">Editar</button>';
-                echo '<button class="clonar"><img src="icons/clonar.svg" alt="Clonar">Clonar</button>';
-                echo '<button class="eliminar"><img src="icons/eliminar.svg" alt="Eliminar">Eliminar</button>';
-                echo '</div>';
-                echo '<div class="imagen-overlay"></div>';
-                echo '</div>';
-            }
-            ?>
-        </div>
+
+        // Generar dinámicamente los divs con class="caja"
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) : ?>
+            <div class="caja">
+                <p><?= htmlspecialchars($row['titulo']) ?></p>
+                <div class="imagen-overlay">
+                    <img src="../icons/ver.png" alt="">
+                </div>
+                <button class="opciones-btn" onclick="mostrarOpciones(event)">+</button>
+                <div class="opciones" id="opciones<?= $row['id'] ?>">
+                    <button class="editar"><img src="icons/editar.svg" alt="Editar">Editar</button>
+                    <button class="clonar"><img src="icons/clonar.svg" alt="Clonar">Clonar</button>
+                    <form action="php/delete/eliminar.php" method="POST" onsubmit="return confirmarEliminacion()">
+                        <input type="hidden" name="presentation_id" value="<?= $row['id'] ?>">
+                        <button type="submit" class="eliminar"><img src="icons/eliminar.svg" alt="Eliminar">Eliminar</button>
+                    </form>
+                </div>
+            </div>
+        <?php endwhile; ?>
+    </div>
 
 
     <script src="js/home.js"></script>
--- js/home.js
+++ js/home.js
@@ -1,22 +1,22 @@
 //Función para mostrar opciones de editar, eliminar y clonar presentación
 function mostrarOpciones(event) {
-    const opciones = event.target.nextElementSibling;
-    if (opciones.style.display === "none" || opciones.style.display === "") {
-        opciones.style.display = "block";
-        event.target.textContent = "-";
-    } else {
-        opciones.style.display = "none";
-        event.target.textContent = "+";
-    }
-}    
+  const opciones = event.target.nextElementSibling;
+  if (opciones.style.display !== 'block') {
+    opciones.style.display = 'block';
+    event.target.textContent = '-';
+  } else {
+    opciones.style.display = 'none';
+    event.target.textContent = '+';
+  }
+}
 
 //Función para mostrar la imagen para visualizar presentaciones cuando el ratón pase por encima de éstas
 function mostrarImagen(c) {
-    const imagenOverlay = c.querySelector('.imagen-overlay');
-    imagenOverlay.style.backgroundImage = 'url("icons/ver.png")';
-    imagenOverlay.style.display = 'block';
+  const imagenOverlay = c.querySelector('.imagen-overlay');
+  imagenOverlay.style.backgroundImage = 'url("icons/ver.png")';
+  imagenOverlay.style.display = 'block';
 }
 function ocultarImagen(c) {
-    const imagenOverlay = c.querySelector('.imagen-overlay');
-    imagenOverlay.style.display = 'none';
+  const imagenOverlay = c.querySelector('.imagen-overlay');
+  imagenOverlay.style.display = 'none';
 }
