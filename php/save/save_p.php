937fea042afba0bb1ec1328bebd4eaf4f1cb0202
Emilio Fernández Vicente <efernandez@alumnat.copernic.cat>
2023-10-30 20:21:04 +0100

    implementando guardar editado

--- php/save/save_p.php
+++ php/save/save_p.php
@@ -15,31 +15,12 @@ $db = Database::getInstance();
 $conn = $db->getConnection();
 
 if (!empty($_POST['presentacion_id'])) {
-    if (Presentacion::exists($conn, $_POST['presentacion_id'])) {
-        while (isset($_POST['d_titulo_' . $count_diapositiva])) {
-            if (isset($_POST['d_contenido_' . $count_diapositiva])) {
-                array_push(
-                    $diapositivas,
-                    new TipoContenido(
-                        $count_diapositiva,
-                        $_POST['d_titulo_' . $count_diapositiva],
-                        $_POST['d_contenido_' . $count_diapositiva]
-                    )
-                );
-            } else {
-                array_push(
-                    $diapositivas,
-                    new TipoTitulo(
-                        $count_diapositiva,
-                        $_POST['d_titulo_' . $count_diapositiva]
-                    )
-                );
-            }
-            $count_diapositiva++;
-        }
+    $presentacionBD = Presentacion::getPresentacionBD($conn, $_POST['presentacion_id']);
 
-        Presentacion::nuevaPresentacionBD($conn, $titulo, $descripcion, $diapositivas);
+    foreach ($diapositivas as $value) {
+        # code...
     }
+    
 } else {
     $titulo = isset($_POST['p_titulo']) ? trim($_POST['p_titulo']) : '';
     $descripcion = isset($_POST['p_descripcion']) ? trim($_POST['p_descripcion']) : '';
